4529b0423d8491da30d7b556851d5484c560e2c6
sandeepvirdi <49703761+sandeepvirdi@users.noreply.github.com>
2019-08-13 13:36:40 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+	</div>
+</div>
+<div class="footer">
+	&copy; <a href="http://www.mydigitalgarden.com">mydigitalgarden</a> 2019. All rights reserved.
+	<span class="right">"Film" graphic courtesy of sandeep virdi at <a href="http://freedigitalphotos.net/images/view_photog.php?photogid=587">freedigitalphotos.net</a></span>
+</div>
+</body>
+</html>
