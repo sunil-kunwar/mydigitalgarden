741446c8e31adc40e26f51d6b92055a9771acdcc
nancyarora <48901907+nancyarora@users.noreply.github.com>
2019-09-06 14:45:43 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
     include 'template/header.php';
     
     if(logged_in()){
-        echo 'Welcome, you can now start <a href="plants.php">Add Plants</a> and <a href="groups.php">create a group of plants</a>';
+      ?> <center style="margin-top:-45px;"><h3>Welcome to Digital Garden</h3></center><?php
+
+      
+        echo '<a href="add_plant.php"> <br><br><br><br><br><li>Add Plant </a><br/><br/><br/><br/>
+         <a href="groups.php"><li>New Group</a>';
     }
     else{
-        echo '<img src="images/landing.png" alt="Register a free account today" />';
+        echo "Register your account today";
     }
     
 ?>
