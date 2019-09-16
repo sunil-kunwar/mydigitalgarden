14a3fdc641ad4802bb3d84e8bac580df26e988cc
nancyarora <48901907+nancyarora@users.noreply.github.com>
2019-09-16 15:51:45 +1000

    Add files via upload

--- add_plant.php
+++ add_plant.php
@@ -15,6 +15,11 @@
         <p>Plant Name:<br><input type="text" name="plant_name" required /></p>
         <p>Plant Scientific Name:<br><input type="text" name="plant_scientific_name" required /></p>
         <p>Number of Plant:<br><input type="text" name="number_of_plant" required /></p>
+        <p>Family:<br><input type="text" name="family" optional /></p>
+        <p>Genera:<br><input type="text" name="genera" optional /></p>
+        <p>Species:<br><input type="text" name="species" optional /></p>
+        <p>Popular Name:<br><input type="text" name="popular name" optional /></p>
+        <p>Comments:<br><input type="text" size="50" optional /></p>
         <p>Image:<br> <input type="file" name="plant_image" id="plant_image" required /></p>
        
         <p><input type="submit"  name="submit" value="Add Plant" /></p>
